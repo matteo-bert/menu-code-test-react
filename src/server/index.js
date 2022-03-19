33933329d99afb4cfc98029ea0aec7e02c021047
Matteo Bertin <bertin.matteo@outlook.com>
2022-03-19 15:43:38 +0100

    Added ESLint and Prettier configs

--- src/server/index.js
+++ src/server/index.js
@@ -10,22 +10,22 @@ const port = 3000;
 const app = express();
 
 app.get('/api/v1/menu', (req, res) => {
-    res.json(menu);
+  res.json(menu);
 });
 
 // enable `cors` to set HTTP response header: Access-Control-Allow-Origin: *
 app.use(cors());
 
 app.use(
-    '/graphql',
-    graphqlHTTP({
-        schema: buildSchema(gqlSchema),
-        rootValue: { menu: () => menu },
-        graphiql: true,
-    })
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(gqlSchema),
+    rootValue: { menu: () => menu },
+    graphiql: true,
+  }),
 );
 
 app.listen(port, () => {
-    console.log(`The API server is running at http://localhost:${port}/api`);
-    console.log(`The GraphQL server is running at http://localhost:${port}/graphql`);
+  console.log(`The API server is running at http://localhost:${port}/api`);
+  console.log(`The GraphQL server is running at http://localhost:${port}/graphql`);
 });
